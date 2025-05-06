b872af9ea0424efbd0b8038b6ee302ea1e22d452
MBATheGamer <mbathegamer@gmail.com>
2025-05-06 11:40:55 +0100

    Add additional loading skeletons

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,3 +1,12 @@
+import { Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 export default () => {
-  return <></>;
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Skeleton height="20rem" />
+    </Box>
+  );
 };
