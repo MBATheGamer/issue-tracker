23db6fb11d07fd9928e151f9b55b1fd2071aa0d5
MBATheGamer <mbathegamer@gmail.com>
2025-05-09 13:28:59 +0100

    Fix: avatar not loading

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  headers: async () => [
+    {
+      source: "/:path*",
+      headers: [{ key: "referrer-policy", value: "no-referrer" }],
+    },
+  ],
 };
 
 export default nextConfig;
